51d64eb9617eee02fc8e1834290303bcc10ffdbc
Emanuel Cioara <flexin199@gmail.com>
2021-10-18 00:09:05 +0300

    Emanuel Cioara Tema2

---
+++ Lab2DacEmanuel/Controllers/StudentsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Repositories;
+using Models;
+
+namespace Lab2.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        public StudentsController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            return StudentsRepo.Students;
+        }
+
+        [HttpGet("{id}")]
+        public Student GetStudent(int id)
+        {
+            return StudentsRepo.Students.FirstOrDefault(student => student.Id == id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Student student)
+        {
+            StudentsRepo.Students.Add(student);
+        }
+
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            StudentsRepo.Students.Remove(StudentsRepo.Students.FirstOrDefault(student => student.Id == id));
+        }
+    }
+}
