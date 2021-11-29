3658d47492646f7912ee061d923ccbc81f71df40
Emanuel Cioara <flexin199@gmail.com>
2021-11-30 01:31:17 +0200

    add function

---
+++ Laborator6/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace Laborator6
+{
+    class Program
+    {
+        const string ServiceBusConnectionString="Endpoint=sb://laborator6cioaraemanuel.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ZTHbpbKQ3U2px+mkapVRAenz+o5yCAM/wecbBMG+PI4=";
+        const string QueueName="coadalab6";
+        static IQueueClient queueClient;
+        public static async Task Main(string[] args)
+        {
+            const int numberOfMessages=10;
+            queueClient=new QueueClient(ServiceBusConnectionString,QueueName);
+
+            Console.WriteLine("=============================================");
+            Console.WriteLine("Press Enter key to exit after sending all messages");
+            Console.WriteLine("=============================================");
+
+            await SendMessagesAsync(numberOfMessages);
+
+            Console.ReadKey();
+
+            await queueClient.CloseAsync();
+
+        }
+
+        static async Task SendMessagesAsync(int numberOfMessagesToSend)
+        {
+            try
+            {
+                for(var i=0;i<numberOfMessagesToSend;i++)
+                {
+                    string messageBody=$"Hello, world: {i}";
+                    var message=new Message(Encoding.UTF8.GetBytes(messageBody));
+                    Console.WriteLine($"Sending message: {messageBody}");
+                    await queueClient.SendAsync(message);
+                }
+            } catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception {exception.Message}");
+            }
+        }
+    }
+}
---
+++ Laborator7/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace Laborator7
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "Endpoint=sb://laborator6cioaraemanuel.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=ZTHbpbKQ3U2px+mkapVRAenz+o5yCAM/wecbBMG+PI4=";
+        const string QueueName = "coadalab6";
+        static IQueueClient queueClient;
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            queueClient = new QueueClient(ServiceBusConnectionString, QueueName);
+
+            Console.WriteLine("=============================================");
+            Console.WriteLine("Press Enter key to exit after receiving all messages");
+            Console.WriteLine("=============================================");
+
+            RegisterOnMessageHandlerAndReceiveMessages();
+
+            Console.ReadKey();
+
+            await queueClient.CloseAsync();
+
+        }
+
+        static void RegisterOnMessageHandlerAndReceiveMessages()
+        {
+            var messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                MaxConcurrentCalls = 1,
+                AutoComplete = false
+            };
+
+            queueClient.RegisterMessageHandler(ProcessMessageAsync, messageHandlerOptions);
+        }
+
+        static async Task ProcessMessageAsync(Message message, CancellationToken token)
+        {
+            Console.WriteLine($"Received message: Sequence number:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+
+            await queueClient.CompleteAsync(message.SystemProperties.LockToken);
+        }
+
+        static Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Console.WriteLine($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}");
+            var context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            Console.WriteLine("Exception context for troubleshooting:");
+            Console.WriteLine($"- Endpoint: {context.Endpoint}");
+            Console.WriteLine($"- Entity path: {context.EntityPath}");
+            Console.WriteLine($"- Executing action: {context.Action}");
+
+            return Task.CompletedTask;
+        }
+    }
+}
